bfd0ac6b6bafebf4f6dfa75bc22ea876be921a5e
Yanbo Ding <yanboding@Yanbos-MacBook-Air.local>
2024-11-18 19:57:45 -0500

    optimal policy_plot

--- numarical_study/policy_plot.py
+++ numarical_study/policy_plot.py
@@ -1,24 +1,34 @@
 from pprint import pprint
-
+'''
+Q_{\theta}(s,a) \in R
+q_network = f(s,a) s_size+a_size -> 1 
+min_q = inf
+best_actoin = None
+for action in env.valid_action(s):
+   q = q_network(s, action)
+   if q < min_q:
+      min_q = q
+      best_actoin = a
+'''
 import numpy as np
 
 from environment import SchedulingEnv
 from decision_maker import OptimalAgent
 from utils import get_system_dynamic
 
-treatment_patterns = np.array([[2,1,0], [1,0,1], [0,1,2]]).T
+treatment_patterns = np.array([[2,1,0], [1,0,1]]).T
 
-arrival_mean = 5
-type_proportion = [0.2, 0.3, 0.5]
+arrival_mean = 3
+type_proportion = [0.2, 0.8]
 system_dynamic = get_system_dynamic(arrival_mean, arrival_mean*3, list(type_proportion))
 
-holding_cost = np.array([10, 5, 8])
+holding_cost = np.array([10, 5])
 overtime_cost = 30
 duration = 1
-regular_capacity = 5
+regular_capacity = 3
 discount_factor = 0.99
 
-decision_epoch = 15
+decision_epoch = 5
 
 env = SchedulingEnv(
              treatment_pattern=treatment_patterns,
@@ -33,8 +43,10 @@ env = SchedulingEnv(
 )
 
 optimal_agent = OptimalAgent(env, discount_factor)
-init_state = (np.array([0,0,0]), np.array([1, 2, 3]))
-optimal_agent.train(init_state, 1)
+init_state = (np.array([0,0,0]), np.array([1, 2]))
+optimal_agent.load('./decision_epoch_5')
+#optimal_agent.train(init_state, 1)
+#optimal_agent.save('.')
 print(optimal_agent.policy(init_state, 1), optimal_agent.get_state_value(init_state, 1))
 '''
 #optimal_agent.load('.')
